7640af0a720f370c9bb955e3101558f6742ab28e
Kevin Casey <kevin>
2024-10-01 11:54:16 -0500

    try new favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,7 +13,7 @@ module.exports = {
     {
       resolve: "gatsby-plugin-manifest",
       options: {
-        icon: "src/images/icon.png",
+        icon: "src/images/android-chrome-512x512.png",
       },
     },
     "gatsby-plugin-mdx",
